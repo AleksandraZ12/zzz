cbfa7e5ff6dc1d5a318cb82c88afd492a08072ea
AleksandraZ12 <teevoids@gmail.com>
2024-10-21 13:01:57 +0000

    Изменено приветствие в index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/hello', (req, res) => {
-    const name = req.query.name || 'World';
-    res.send(`Hello, ${name}`);
-});
-
-console.log('Greetings from the main branch!');
-console.log('Hello Wield!');
-
-app.listen(port, () => {
-    console.log(`App is running at http://localhost:${port}`);
-});
+console.log('Привет, мир\!');
